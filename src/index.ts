cb5a9a077d44ac76d9c1dec9de25cd300e2a801f
Paula <paulakarlin.pk@gmail.com>
2021-04-20 00:00:45 +0200

    Hinzugefügt: Com.,Set + checkWin +umstruktursetzeX

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,57 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
 
-import { validateWin } from "./myModule";
+import { computerSet } from "./myModule";
 import { alertMe, } from "./myOtherModule";
 
 console.log("es cuadasd")
-document.querySelector("#test") ?.addEventListener("click", put);
-document.querySelector("#test2") ?.addEventListener("click", put1);
-document.querySelector("#test3") ?.addEventListener("click", put2);
+document.querySelector("#test") ?.addEventListener("click", setzeX);
+document.querySelector("#test2") ?.addEventListener("click", setzeX);
+document.querySelector("#test3") ?.addEventListener("click", setzeX);
 export let checkVar1:number;
 export let checkVar2:number;
 export let checkVar3:number;
+export let feld1:number;
 init();
 
 function init(){
- 
 checkVar1 =0;
 checkVar2 =0;
 checkVar3 =0;
+
+console.log(checkVar1)
   alertMe();
-
-validateWin();
-
 }
 
-    function put() {
-
- console.log("es cuadasd")
- console.log(checkVar1)
- if(checkVar1 ===0){
-  checkVar1 ++;
+function setzeX(){
+ if(document.querySelector("#test"))
+ {
+   if(checkVar1 ===0){
+  //checkVar1 ++;
   console.log(checkVar1)
    // 1. Select the div element using the id property
-const app = document.getElementById("test");
+let app = document.getElementById("test");
 // 2. Create a new <p></p> element programmatically
 const p = document.createElement("p");
 // 3. Add the text content
 p.textContent = "X";
 // 4. Append the p element to the div element //wichtig
 app?.appendChild(p);
-validateWin();
+feld1 = 1;
+computerSet();
 
+ }
 
-  } 
-}
-function put1() {
-
-  console.log("es cuadasd")
+else if(document.querySelector("#test2")){
   if(checkVar2 ===0){
     checkVar2 ++;
     console.log(checkVar2)
-    // 1. Select the div element using the id property
  const app = document.getElementById("test2");
- // 2. Create a new <p></p> element programmatically
  const p = document.createElement("p");
- // 3. Add the text content
  p.textContent = "X";
- // 4. Append the p element to the div element //wichtig
  app?.appendChild(p);
- validateWin();
 
-   } 
  }
- function put2() {
-
-  console.log("es cuadasd")
+ else if(document.querySelector("#test3")){
   if(checkVar3 ===0){
     checkVar3 ++;
     console.log(checkVar3)
@@ -75,9 +63,22 @@ function put1() {
  p.textContent = "X";
  // 4. Append the p element to the div element //wichtig
  app?.appendChild(p);
- validateWin();
-
-   } 
  }
+
+}
+ }
+}
+validateWin();
+
+}
+function validateWin()
+{
+  if(checkVar1===1 &&checkVar2 ===1 &&checkVar3===1)
+  {
+   alert("you won")
+
+  }
+
+}
+
  
-   
--- src/myModule.ts
+++ src/myModule.ts
@@ -1,12 +1,31 @@
 // THIS IS A MODULE
 
+import { checkVar1, checkVar2, checkVar3, feld1 } from "./index";
+let enemyChoice:number;
+const felder =[1,2,3,4,5,6,7,8,9]
 export function alertMe(): void {
   alert("ATTENTION PLEASE");
 }
 
 export function computerSet(){
   console.log("asdasd3454")
+  enemyChoice=felder[getRandomInt(0,1)]
+  console.log(enemyChoice)
+  if(checkVar1=== 0 && enemyChoice===feld1){
+    let app = document.getElementById("test");
+// 2. Create a new <p></p> element programmatically
+const p = document.createElement("p");
+// 3. Add the text content
+p.textContent = "O";
+// 4. Append the p element to the div element //wichtig
+app?.appendChild(p);
+
+  }
 }
+function getRandomInt(min:number, max:number):number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export class Feld
 {
   static innerHTML: any;
@@ -14,9 +33,7 @@ export class Feld
    h(heading: string)
 {
  
- /* function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}*/
+ 
   
   ;
 
