c32c5eaa7883a4f0bca3502eecbe9d97b7802dee
Paula <paulakarlin.pk@gmail.com>
2021-04-28 21:56:58 +0200

    soweit funktionsfähig : )

--- src/myModule.ts
+++ src/myModule.ts
@@ -1,7 +1,7 @@
 // THIS IS A MODULE
 
 import { } from "./index";
-import { setWin, validateWin } from "./myOtherModule";
+import { validateWinHuman, validateWinPC } from "./myOtherModule";
 let enemyChoice:number;
 const felder =[1,2,3,4,5,6,7,8,9]
 export var checkVar1:boolean;
@@ -14,6 +14,8 @@ export var checkVar7:boolean;
 export var checkVar8:boolean;
 export var checkVar9:boolean;
 
+export let app1 = document.getElementById("test2");
+
 export var feld1:boolean;
 export var feld2:boolean;
 export var feld3:boolean;
@@ -24,15 +26,26 @@ export var feld7:boolean;
 export var feld8:boolean;
 export var feld9:boolean;
 
-let enemySet1:boolean;
-let enemySet2:boolean;
-let enemySet3:boolean;
-let enemySet4:boolean;
-let enemySet5:boolean;
-let enemySet6:boolean;
-let enemySet7:boolean;
-let enemySet8:boolean;
-let enemySet9:boolean;
+export let enemySet1:boolean;
+export let enemySet2:boolean;
+export let enemySet3:boolean;
+export let enemySet4:boolean;
+export let enemySet5:boolean;
+export let enemySet6:boolean;
+export let enemySet7:boolean;
+export let enemySet8:boolean;
+export let enemySet9:boolean;
+
+let feldnr1:number;
+let feldnr2:number;
+let feldnr3:number;
+let feldnr4:number;
+let feldnr5:number;
+let feldnr6:number;
+let feldnr7:number;
+let feldnr8:number;
+let feldnr9:number;
+
 
 export function alertMe(): void {
   alert("ATTENTION PLEASE");
@@ -40,98 +53,112 @@ export function alertMe(): void {
 
 export function computerSet(){
   console.log("computer setzt")
-  enemyChoice=felder[getRandomInt(0,3)]
+  enemyChoice=felder[getRandomInt(1,9)]
   console.log(enemyChoice)
   let i:number;
   if(i=2, i=2,i--){
-    if(enemyChoice === 1 && feld1===false &&checkVar1===false) {
+    if(enemyChoice === 1 && feld1===false && feldnr1 != enemyChoice) {
       let app1 = document.getElementById("test");
-      const p = document.createElement("p");
+      let p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld1= true;
+      validateWinPC();
       //noch ne "tolle" hilfsvar
    
       enemySet1 = true;
       }
-     else if(enemyChoice === 2 && feld2===false &&checkVar2===false) {
-      let app1 = document.getElementById("test2");
+     else if(enemyChoice === 2 && feld2===false  && feldnr2 != enemyChoice) {
+      
       const p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld2= true;
        enemySet2 = true;
+       validateWinPC();
       
     }
-    else if(enemyChoice === 3 && feld3===false &&checkVar3===false) {
+    else if(enemyChoice === 3 && feld3===false && feldnr3 != enemyChoice) {
       let app1 = document.getElementById("test3");
       const p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld3= true;
        enemySet3 = true;
+       validateWinPC();
       
     }
-    else if(enemyChoice === 4 && feld4===false &&checkVar4===false) {
+    else if(enemyChoice === 4 && feld4===false  && feldnr4 != enemyChoice) {
       let app1 = document.getElementById("test4");
       const p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld4= true;
        enemySet4 = true;
+       validateWinPC();
     }
 
-    else if(enemyChoice === 5 && feld5===false &&checkVar5===false) {
+    else if(enemyChoice === 5 && feld5===false && feldnr5 != enemyChoice) {
       let app1 = document.getElementById("test5");
       const p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld5= true;
        enemySet5 = true;
+       validateWinPC();
     }
-    else if(enemyChoice === 6 && feld6===false &&checkVar6===false) {
+    else if(enemyChoice === 6 && feld6===false && feldnr6 != enemyChoice) {
       let app1 = document.getElementById("test6");
       const p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld6= true;
        enemySet6 = true;
+       validateWinPC();
     }
-    else if(enemyChoice === 7 && feld7===false &&checkVar7===false) {
+    else if(enemyChoice === 7 && feld7===false  && feldnr7 != enemyChoice) {
       let app1 = document.getElementById("test7");
       const p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld7= true;
        enemySet7 = true;
+       validateWinPC();
     }
-    else if(enemyChoice === 8 && feld8===false &&checkVar8===false) {
+    else if(enemyChoice === 8 && feld8===false  && feldnr8 != enemyChoice) {
       let app1 = document.getElementById("test8");
       const p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld8= true;
        enemySet8 = true;
+       validateWinPC();
     }
-    else if(enemyChoice === 9 && feld9===false &&checkVar9===false) {
+    else if(enemyChoice === 9 && feld9===false && feldnr9 != enemyChoice) {
       let app1 = document.getElementById("test9");
       const p = document.createElement("p");
       p.textContent = "O";
       app1?.appendChild(p);
       feld9= true;
       enemySet9 = true;
+      validateWinPC();
     }
     else if(((feld1&&feld2&&feld3&&feld4&&feld5&&feld6&&feld7&&feld8&&feld9)||(checkVar1&&checkVar2&&checkVar3&&
       checkVar4&&checkVar5&&checkVar6&&checkVar7&&checkVar8&&checkVar9))===true){
      
-      
+      alert("DRAW")
+    }
+    else if(feldnr1 ===enemyChoice || feldnr3=== enemyChoice || feldnr2 === enemyChoice){
+      console.log("der comp hat die versucht ins gegnerfeld zu setzen")
+      computerSet();
     }
     else if((enemySet1||enemySet2||enemySet3||enemySet4||enemySet5||enemySet6||enemySet7||enemySet8||enemySet9)===true){
-      if(setWin===false){
+     
         console.log("DER PC HAT 2 MAL DIE GLEICHE ZAHL")
         computerSet();
 
-      }
+      
+      
      
      
       
@@ -140,78 +167,7 @@ export function computerSet(){
     
     
      
-    
-    
 
-
-/*case 2 ||feld2===false: 
-  let app2 = document.getElementById("test2");
-const p2 = document.createElement("p");
-p2.textContent = "O";
-app2?.appendChild(p2);
-feld2===true;
-break;
-
-case 3 || feld3===false :
-  let app3 = document.getElementById("test3");
-  const p3 = document.createElement("p");
-  p3.textContent = "O";
-  app3?.appendChild(p3);
-  feld3===true;
-  break;
-
-  case 4 ||feld4===false:
-  let app4 = document.getElementById("test4");
-  const p4 = document.createElement("p");
-  p4.textContent = "O";
-  app4?.appendChild(p4);
-  feld4===true;
-  break;
-
-  case 5 ||feld5===false:
-  let app5 = document.getElementById("test5");
-  const p5 = document.createElement("p");
-  p5.textContent = "O";
-  app5?.appendChild(p5);
-  feld5===true;
-  break;
-
-  case 6 ||feld6===false :
-  let app6 = document.getElementById("test6");
-  const p6 = document.createElement("p");
-  p6.textContent = "O";
-  app6?.appendChild(p6);
-  feld6===true;
-  break;
-
-  case 7 ||feld7===false:
-    let app7 = document.getElementById("test7");
-    const p7 = document.createElement("p");
-    p7.textContent = "O";
-    app7?.appendChild(p7);
-    feld7===true;
-    break;
-
-    case 8 ||feld8===false:
-    let app8 = document.getElementById("test8");
-    const p8 = document.createElement("p");
-    p8.textContent = "O";
-    app8?.appendChild(p8);
-    feld8===true;
-    break;
-
-    case 9 ||feld9===false:
-    let app9 = document.getElementById("test9");
-    const p9 = document.createElement("p");
-    p9.textContent = "O";
-    app9?.appendChild(p9);
-    feld9===true;
-    break;
-
-    case undefined || (checkVar1||checkVar2||checkVar3||checkVar4||checkVar5||checkVar6||checkVar7||checkVar8||checkVar9||feld1||feld2||feld3||feld4||feld5||feld6||feld7||feld8||feld9)===true:
-      computerSet()
-      break;
-  } */
 }
 
 
@@ -219,21 +175,7 @@ export function getRandomInt(min:number, max:number):number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export class Feld
-{
-  static innerHTML: any;
-  constructor(private container: HTMLImageElement){}
-   h(heading: string)
-{
- 
- 
-  
-  ;
 
-
-
-}
-}
 export function init(){
   feld1 = false;
   feld2 = false;
@@ -253,12 +195,9 @@ export function init(){
   checkVar7=false;
   checkVar8=false;
   checkVar9=false;
-
-
-
   
 
-console.log(checkVar1)
+
   alertMe();
 }
 
@@ -267,6 +206,7 @@ export function put1() {
   console.log(checkVar1)
   if(feld1===false&& checkVar1===false){
    checkVar1 = true;
+   feldnr1 =1 ;
    feld1=true;
    console.log(checkVar1)
     // 1. Select the div element using the id property
@@ -277,7 +217,7 @@ export function put1() {
  p.textContent = "X";
  // 4. Append the p element to the div element //wichtig
  app?.appendChild(p);
- validateWin();
+ validateWinHuman();
  computerSet();
  
    } 
@@ -286,13 +226,14 @@ export function put1() {
    console.log("setze in feld 2 ein X")
    if(feld2===false && checkVar2=== false){
      checkVar2 =true;
+     feldnr2 =2 ;
      feld2=true;
      console.log(checkVar2)
   const app = document.getElementById("test2");
   const p = document.createElement("p");
   p.textContent = "X";
   app?.appendChild(p);
-  validateWin();
+  validateWinHuman();
   computerSet();
  
     } 
@@ -302,13 +243,14 @@ export function put1() {
    console.log("setze in feld 3 ein X")
    if(feld3=== false && checkVar3=== false){
      checkVar3 =true;
+     feldnr3 =3 ;
      feld3=true;
      console.log(checkVar3)
   const app = document.getElementById("test3");
   const p = document.createElement("p");
   p.textContent = "X";
   app?.appendChild(p);
-  validateWin();
+  validateWinHuman();
   computerSet();
     } 
   }
@@ -316,6 +258,7 @@ export function put1() {
   export function put4() {
     console.log("es cuadasd")
     if(feld4=== false && checkVar4=== false){
+      feldnr4 =4 ;
       checkVar4 =true;
       feld4=true;
       console.log(checkVar4)
@@ -323,7 +266,7 @@ export function put1() {
    const p = document.createElement("p");
    p.textContent = "X";
    app?.appendChild(p);
-   validateWin();
+   validateWinHuman();
    computerSet();
   
      } 
@@ -332,13 +275,14 @@ export function put1() {
     console.log("es cuadasd")
     if(feld5=== false && checkVar5=== false){
       checkVar5 =true;
+      feldnr5 =5 ;
       feld5=true;
       console.log(checkVar5)
    const app = document.getElementById("test5");
    const p = document.createElement("p");
    p.textContent = "X";
    app?.appendChild(p);
-   validateWin();
+   validateWinHuman();
    computerSet();
   
      } 
@@ -348,18 +292,20 @@ export function put1() {
     if(feld6=== false&& checkVar6=== false){
       feld6=true;
       checkVar6 = true;
+      feldnr6 =6 ;
       console.log(checkVar6)
    const app = document.getElementById("test6");
    const p = document.createElement("p");
    p.textContent = "X";
    app?.appendChild(p);
-   validateWin();
+   validateWinHuman();
    computerSet();
      } 
    }
    export function put7() {
     console.log("es cuadasd")
     if(feld7=== false && checkVar7=== false){
+      feldnr7 =7 ;
       checkVar7 =true;
       feld7=true;
       console.log(checkVar7)
@@ -367,7 +313,7 @@ export function put1() {
    const p = document.createElement("p");
    p.textContent = "X";
    app?.appendChild(p);
-   validateWin();
+   validateWinHuman();
    computerSet();
   
      } 
@@ -376,6 +322,7 @@ export function put1() {
    export function put8() {
     console.log("setze in feld 8 ein X")
     if(feld8=== false && checkVar8=== false){
+      feldnr8 =8 ;
       checkVar8 = true;
       feld8=true;
       console.log(checkVar8)
@@ -383,7 +330,7 @@ export function put1() {
    const p = document.createElement("p");
    p.textContent = "X";
    app?.appendChild(p);
-   validateWin();
+   validateWinHuman();
    computerSet();
   
      } 
@@ -391,6 +338,7 @@ export function put1() {
    export function put9() {
     console.log("es cuadasd")
     if(feld9=== false && checkVar9=== false){
+      feldnr9 =9 ;
       checkVar9 = true;
       feld9=true;
       console.log(checkVar9)
@@ -398,17 +346,12 @@ export function put1() {
    const p = document.createElement("p");
    p.textContent = "X";
    app?.appendChild(p);
-   validateWin();
+   validateWinHuman();
    computerSet();
   
      } 
    }
 
-   /*function resert(){
-     let reset = document.getElementById("test"&&"test1"&&"test3"&&
-     "test4"&&"test5"&&"test6"&&"test7"&&"test8"&&"test9")
-    // const p = document.removeElement("p");
- 
-   }*/
+
 
 
--- src/myOtherModule.ts
+++ src/myOtherModule.ts
@@ -1,66 +1,108 @@
 // THIS IS A MODULE
 
-import { feld1, feld2, feld3, feld4, feld5, feld6, feld7, feld8, feld9, checkVar1, checkVar2, checkVar3, checkVar4, checkVar5, checkVar6, checkVar7, checkVar8, checkVar9 } from "./myModule";
+import { feld1, feld2, feld3, feld4, feld5, feld6, feld7, feld8, feld9, checkVar1, checkVar2, checkVar3, checkVar4, checkVar5, checkVar6, checkVar7, checkVar8, checkVar9, enemySet1, enemySet2, enemySet3, enemySet4, enemySet5, enemySet6, enemySet7, enemySet8, enemySet9 } from "./myModule";
 export let setWin:boolean;
 export function alertMe(): void {
   alert("ATTENTION PLEASE");
 }
-export function validateWin()
+export function validateWinHuman() //Hier wird geprüft ob der Mensch gewonnen hat
 {
   switch(feld1||feld2||feld3||feld4||feld5||feld6||feld7||feld8||feld9)
   {
     //horizontale wins
-   case (feld1&&feld2&&feld3&&
-    checkVar1&&checkVar2&&checkVar3)===true:
+   case (checkVar1&&checkVar2&&checkVar3)===true:
    alert("you won mit feld1+2+3")
-setWin=true;
+
   
    break;
 
 
-   case (feld4&&feld5&&feld6&&
-    checkVar4&&checkVar5&&checkVar6)===true:
+   case (checkVar4&&checkVar5&&checkVar6)===true:
     alert("you won mit feld1+4+7")
-    setWin=true;
+
     break;
 
-    case (feld7&&feld8&&feld9&&
-      checkVar7&&checkVar8&&checkVar9)===true:
+    case ( checkVar7&&checkVar8&&checkVar9)===true:
     alert("you won mit feld1+4+7")
-    setWin=true;
+
     break;
 //senkrechte wins
-   case (feld1&&feld4&&feld7&&
-    checkVar1&&checkVar4&&checkVar7)===true:
+   case (checkVar1&&checkVar4&&checkVar7)===true:
     alert("you won mit feld1+4+7")
-    setWin=true;
+
     break;
 
-    case (feld2&&feld5&&feld8
-      &&checkVar2&&checkVar5&&checkVar8)===true:
+    case (checkVar2&&checkVar5&&checkVar8)===true:
       alert("you won mit feld1+4+7")
-      setWin=true;
+
       break;
 
-      case (feld3&&feld6&&feld9
-        &&checkVar3&&checkVar6&&checkVar9)===true:
+      case (checkVar3&&checkVar6&&checkVar9)===true:
       alert("you won mit feld1+4+7")
-      setWin=true;
+
       break;
 //diagonale wins
-      case (feld1&&feld5&&feld9
-        &&checkVar1&&checkVar5&&checkVar9)===true:
+      case (checkVar1&&checkVar5&&checkVar9)===true:
         alert("you won mit feld1+4+7")
-        setWin=true;
+
         break;
 
-        case (feld3&&feld5&&feld7
-          &&checkVar3&&checkVar5&&checkVar7)===true:
+        case (checkVar3&&checkVar5&&checkVar7)===true:
           alert("you won mit feld1+4+7")
-          setWin=true;
+
           break;
 
+  }
+  
+}
+
+export function validateWinPC() //Hier wird geprüft ob der Mensch gewonnen hat
+{
+  switch(enemySet1|| enemySet2|| enemySet3|| enemySet4|| enemySet5|| enemySet6|| enemySet7|| enemySet8|| enemySet9)
+  {
+    //horizontale wins
+   case (enemySet1&&enemySet2&&enemySet3)===true:
+   alert("you won mit feld1+2+3")
+
+  
+   break;
+
+
+   case (enemySet4&&enemySet5&&enemySet6)===true:
+    alert("TocRobo won mit feld 4+5+6")
+
+    break;
+
+    case ( enemySet7&&enemySet8&&enemySet9)===true:
+    alert("TocRobo won mit feld7+8+9")
+
+    break;
+//senkrechte wins
+   case (enemySet1&&enemySet4&&enemySet7)===true:
+    alert("TocRobo won mit feld1+4+7")
+
+    break;
+
+    case (enemySet2&&enemySet5&&enemySet8)===true:
+      alert("you won mit feld1+4+7")
+
+      break;
+
+      case (enemySet3&&enemySet6&&enemySet9)===true:
+      alert("you won mit feld1+4+7")
+
+      break;
+//diagonale wins
+      case (enemySet1&&enemySet5&&enemySet9)===true:
+        alert("TocRobo won mit feld1+5+9")
+
+        break;
+
+        case (enemySet3&&enemySet5&&enemySet7)===true:
+          alert("TocRobo won mit feld 3+5+7")
+
+          break;
 
   }
-
+  
 }
